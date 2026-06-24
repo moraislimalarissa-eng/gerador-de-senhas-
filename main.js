be73e9876cf80dbce28dc4aeeb6487037f2812e7
moraislimalarissa-eng <morais.lima.larissa@escola.pr.gov.br>
2026-06-24 10:30:27 -0300

    ajuistes img

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector('.forca');
+const forcaSenha = document.querySelector('.4" alt="caforca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
